81ee325e22903d7d94e6d70c0003b3f3b7c7da2f
Tadhg N <tadhgnolan@gmail.com>
2021-07-14 15:50:37 +0000

    Correct comment error in responsive.js

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,5 +1,5 @@
-/* Toggle adding and removing "responsive" class to topnav when user clicks icon */
-function myFunction() {
+//Toggle adding and removing "responsive" class to topnav when user clicks icon 
+function responsiveIcon() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += "responsive";
