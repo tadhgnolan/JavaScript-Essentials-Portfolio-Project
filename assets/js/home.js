d73d25faf2d598f142ad34d26c9dcebde5fc27a6
Tadhg N <tadhgnolan@gmail.com>
2021-07-14 12:34:33 +0000

    Add JS for responsive topnav

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+/* Toggle adding and removing "responsive" class to topnav when user clicks icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
