8bb2ccc0d1406a1aa90fa326c810c0b6df7cbfdc
Tadhg N <tadhgnolan@gmail.com>
2021-07-15 04:05:58 +0000

    Remove responsive.js

--- assets/js/responsive.js
+++
@@ -1,9 +0,0 @@
-//Toggle adding and removing "responsive" class to topnav when user clicks icon 
-function responsiveIcon() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += "responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
